19cb9556118a0a6a3264bc4b30e0fedbc9202ac8
ilis137 <iliyasshahapure1@gmail.com>
2023-02-14 14:01:53 +0530

    add:open bridgelabz site with event binding

--- src/app/hello-component/hello-component.component.ts
+++ src/app/hello-component/hello-component.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello-component.component.css']
 })
 export class HelloComponentComponent implements OnInit{
-  
+
   message:string = 'Hello World';
   imgUrl:string = "assets/BridgeLabz_New_Logo.svg"
+  url:string = "https://www.bridgelabz.com";
   ngOnInit(): void {
     this.message="Hello from bridgelabz";
+  }
+
+  handleClick(event:any) {
+    console.log("bridgelabz logo is clicked",event);
+    window.open(this.url,"_blank")
   }
 }
