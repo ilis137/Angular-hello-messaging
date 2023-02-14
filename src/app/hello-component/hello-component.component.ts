caf8eb2e960db77ed774592256ba35245baa3437
ilis137 <iliyasshahapure1@gmail.com>
2023-02-14 14:31:56 +0530

    add:display hello message with two way binding

--- src/app/hello-component/hello-component.component.ts
+++ src/app/hello-component/hello-component.component.ts
@@ -10,6 +10,7 @@ export class HelloComponentComponent implements OnInit{
   message:string = 'Hello World';
   imgUrl:string = "assets/BridgeLabz_New_Logo.svg"
   url:string = "https://www.bridgelabz.com";
+  userName: string="";
   ngOnInit(): void {
     this.message="Hello from bridgelabz";
   }
@@ -17,5 +18,8 @@ export class HelloComponentComponent implements OnInit{
   handleClick(event:any) {
     console.log("bridgelabz logo is clicked",event);
     window.open(this.url,"_blank")
+  }
+  get customMessage(){
+    return `Hello ${this.userName} from bridgelabz`;
   }
 }
